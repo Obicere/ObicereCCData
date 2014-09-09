cc2b4adc71661b4b08625f92b7944e4352caebb1
Obicere <mattwzawislak@gmail.com>
2014-09-08 19:24:12 -0600

    Fixed Runner format, cleaned up a few code smells and fixed a written issue.

--- src/GroupRecursionRunner.java
+++ src/GroupRecursionRunner.java
@@ -4,9 +4,9 @@ import org.obicere.cc.tasks.projects.Parameter;
 import org.obicere.cc.tasks.projects.Runner;
 
 @Manifest(author = "Obicere",
-        description = "Return true if any combination of the numbers can reach the target.\nStart will always be 0 to begin with.\nRemember that this is recursive, so no loops.",
-        difficulty = 4,
-        version = 1.0)
+          description = "Return true if any combination of the numbers can reach the target.\nStart will always be 0 to begin with.\nRemember that this is recursive, so no loops.",
+          difficulty = 4,
+          version = 1.0)
 public class GroupRecursionRunner extends Runner {
 
     private static final Parameter[] PARAMETERS = new Parameter[]{
@@ -16,11 +16,11 @@ public class GroupRecursionRunner extends Runner {
     };
 
     @Override
-    public Case[] getCases(){
+    public Case[] getCases() {
         final Case[] cases = new Case[10];
-        for(int i = 0; i < 10; i++){
+        for (int i = 0; i < 10; i++) {
             final int[] nums = new int[random.nextInt(3, 7)];
-            for(int j = 0; j < nums.length; j++){
+            for (int j = 0; j < nums.length; j++) {
                 nums[j] = random.nextInt(-12, 13);
             }
             final int target = random.nextInt(-23, 22);
@@ -30,23 +30,24 @@ public class GroupRecursionRunner extends Runner {
     }
 
     @Override
-    public Parameter[] getParameters(){
+    public Parameter[] getParameters() {
         return PARAMETERS;
     }
 
     @Override
-    public String getMethodName(){
+    public String getMethodName() {
         return "group";
     }
 
     @Override
-    public Class<?> getReturnType(){
+    public Class<?> getReturnType() {
         return boolean.class;
     }
 
-	private boolean groupSum(int start, int target, int[] nums) {
-		if (start == nums.length)
-			return target == 0;
-		return (groupSum(start + 1, target - nums[start], nums)) || groupSum(start + 1, target, nums);
-	}
+    private boolean groupSum(int start, int target, int[] nums) {
+        if (start == nums.length) {
+            return target == 0;
+        }
+        return groupSum(start + 1, target - nums[start], nums) || groupSum(start + 1, target, nums);
+    }
 }
