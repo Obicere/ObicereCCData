fbf2a02edcd25f046d7733e0abe734376c111800
Obicere <mattwzawislak@gmail.com>
2014-10-01 11:11:24 -0600

    Lets not return array - increases readability of the parameters.

--- src/LargeArrayRunner.java
+++ src/LargeArrayRunner.java
@@ -4,9 +4,9 @@ import org.obicere.cc.projects.Parameter;
 import org.obicere.cc.projects.Runner;
 
 @RunnerManifest(author = "Obicere",
-          description = "Return the array that has a larger sum out of the 2.\n{1, 3, 2}, {5, 3, 2} -> {5, 3, 2}\n{1, 5, 6}, {4, 8, 0} -> {1, 5, 6}\nThey will always be of same length.\nIf they are of same size, return array 'b'",
-          difficulty = 1,
-          version = 1.0)
+                description = "Return the sum of the array that has a larger sum out of the 2.\n{1, 3, 2}, {5, 3, 2} -> sum({5, 3, 2}) -> 10\n{1, 5, 6}, {4, 8, 0} -> sum({1, 5, 6}) -> 12\nThey will always be of same length.\nIf they are of same size, return 0.",
+                difficulty = 1,
+                version = 1.0)
 public class LargeArrayRunner extends Runner {
 
     private static final Parameter[] PARAMETERS = new Parameter[]{
@@ -22,16 +22,26 @@ public class LargeArrayRunner extends Runner {
             final int[] a = new int[length];
             final int[] b = new int[length];
 
-            int counter = 0;
+            int aSum = 0;
+            int bSum = 0;
             for (int j = 0; j < length; j++) {
                 final int nextA = random.nextInt(-50, 200);
                 final int nextB = random.nextInt(-50, 200);
-                counter += nextA - nextB;
+                aSum += nextA;
+                bSum += nextB;
 
                 a[j] = nextA;
                 b[j] = nextB;
             }
-            cases[i] = new Case(counter > 0 ? a : b, a, b);
+            final int value;
+            if (aSum > bSum) {
+                value = aSum;
+            } else if (aSum < bSum) {
+                value = bSum;
+            } else {
+                value = 0;
+            }
+            cases[i] = new Case(value, a, b);
         }
         return cases;
     }
@@ -48,6 +58,6 @@ public class LargeArrayRunner extends Runner {
 
     @Override
     public Class<?> getReturnType() {
-        return int[].class;
+        return int.class;
     }
 }
